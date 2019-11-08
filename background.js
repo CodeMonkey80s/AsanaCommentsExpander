84954503ebcbf25f3d6ffd28f151cb7fbd747d57
RetroCoder80s <RetroCoder80s@gmail.com>
2019-11-08 09:03:31 +0100

    Fix for not expanding comments upon providing a direct link

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+chrome.tabs.onCreated.addListener(function(tab) {
+    chrome.tabs.sendMessage(tab.tabId, { "message": "initializeExpandAction" });
+});
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     chrome.tabs.sendMessage(tabId, { "message": "initializeExpandAction" });
 });
